0eb846df709a05437b476d90f0f32ee13515ac5f
Daniel Gizzi <mut_dgizzi@externos.entel.cl>
2023-08-07 09:36:51 -0400

    change card projects

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -9,7 +9,7 @@ export const AboutMe = () => {
                         href="/pdf/CV_Daniel_Gizzi.pdf"
                         target="_blank"
                         className="flex items-center justify-center xxs:text-sm xs:text-lg sm:text-lg font-bold border-[1px] rounded-full py-3 w-48 duration-300  border-cyan-500 bg-cyan-500 text-white  hover:border-indigo-600 hover:bg-indigo-600">
-                    CV Daniel Gizzixdd</a>
+                    CV Daniel Gizzi</a>
                 </div>
                 <h2 id="technologies" className="text-4xl text-cyan-400 font-semibold">About me</h2>
                 <p className="text-xl text-slate-200 mt-10 ">I'm a Front End Developer, I am an autodidact <span className="md:block lg:block xl:block">that effort day-to-day to improve my knowledge in the field.</span></p>
