7a668ea26446cb4fb7a015864846399422113501
Daniel Gizzi <mut_dgizzi@externos.entel.cl>
2023-08-29 21:55:01 -0400

    add project Weather app

--- src/data/data-projects.js
+++ src/data/data-projects.js
@@ -4,11 +4,20 @@ export const myProjects = [
         name: 'Techzzi', 
         technologies: ['html', 'css3', 'javascript', 'react', 'tailwindcss', 'sass', 'redux'], 
         page: 'https://danixcabe.github.io/techzzi/', 
-        repository: 'https://github.com/DanixCabe/techzzi'},
+        repository: 'https://github.com/DanixCabe/techzzi'
+    },
     {
         image: 'todoapp',  
         name: 'To Do List', 
         technologies: ['html', 'css3', 'javascript', 'react', 'tailwindcss', 'redux'], 
         page: 'https://danixcabe.github.io/todoapp/', 
-        repository: 'https://github.com/DanixCabe/todoapp'},
+        repository: 'https://github.com/DanixCabe/todoapp'
+    },
+    {
+        image: 'weatherapp',  
+        name: 'Weather App', 
+        technologies: ['css3', 'react', 'tailwindcss', 'redux'], 
+        page: 'https://expo.dev/artifacts/eas/t47wUTRYrWX1uNHZZVygpn.apk', 
+        repository: 'https://github.com/DanixCabe/weather-app'
+    },
 ];
